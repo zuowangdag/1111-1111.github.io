65cfec3c16fb2e41639be251c1d9563a1f5e7fab
zuowangdag <162545828+zuowangdag@users.noreply.github.com>
2024-11-25 21:46:19 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,7 @@
 import asyncio
 import websockets
-import os
 
-# 存储所有连接的 WebSocket 客户端
 connected_clients = set()
-
-# 用于保存文本消息的文件
 text_file_path = "received_texts.txt"
 
 async def echo(websocket, path):
@@ -13,26 +9,19 @@ async def echo(websocket, path):
     try:
         async for message in websocket:
             if isinstance(message, str):
-                # 如果是字符串类型，说明是文本消息
-                print("Received text from client:", message)  # 打印接收到的文本消息
-                # 将文本消息存储在文件中
-                with open(text_file_path, "a") as f:  # 以追加模式打开文件
-                    f.write(message + "\n")  # 每条消息换行存储
-                # 不转发文本消息到客户端
+                print("Received text from client:", message)
+                with open(text_file_path, "a") as f:
+                    f.write(message + "\n")
             else:
-                # 处理图像数据（即二进制消息）
-                print("Received image frame from client")  # 打印接收到的图像数据
-                # 将接收到的视频帧转发给所有连接的客户端
+                print("Received image frame from client")
                 for client in connected_clients:
                     await client.send(message)
-
     finally:
         connected_clients.remove(websocket)
 
-# WebSocket 服务器
 async def server():
-    async with websockets.serve(echo, "2409:891b:f050:479:34d6:8722:7c3a:b794", 8081):
-        await asyncio.Future()  # run forever
+    async with websockets.serve(echo, "0.0.0.0", 8081):
+        await asyncio.Future()
 
 if __name__ == "__main__":
     asyncio.run(server())
